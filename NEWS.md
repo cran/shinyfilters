4bfffc50920d4ae738448d6e58542c274127748c
Josh Livingston <joshwlivingston@gmail.com>
2026-09-15 04:40:02 +0000

    version 0.3.1

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,12 @@
+# shinyfilters 0.3.1
+
+* `get_filter_logical()` now returns `rep(T, length(x))` when a method is not 
+   defined. This behavior is a bandaid for the underlying issue:
+   `numericInput()` returns a logical when the user blanks out the input.
+* Fixes issue in `get_filter_logical()` that caused an error when an argument 
+  was provided to `apply_filters()` and a categorical filter had an active 
+  selection.
+
 # shinyfilters 0.3.0
 
 ## Additions
--- R/get_filter_logical.R
+++ R/get_filter_logical.R
@@ -63,6 +63,15 @@ method(get_filter_logical, list(x = NULL, val = class_any)) <- function(
 	return(NULL)
 }
 
+method(get_filter_logical, list(x = class_any, val = class_any)) <- function(
+	x,
+	val,
+	...
+) {
+	warning("Fallback method called for mismatched types. Returning TRUE")
+	return(rep(TRUE, length(x)))
+}
+
 method(
 	get_filter_logical,
 	list(x = class_data.frame, val = class_any)
@@ -86,7 +95,7 @@ method(
 		x = class_character | class_factor | class_logical,
 		val = class_character | class_factor | class_logical
 	)
-) <- function(x, val) {
+) <- function(x, val, ...) {
 	na_bool <- NULL
 	if (anyNA(val)) {
 		na_bool <- is.na(x)
